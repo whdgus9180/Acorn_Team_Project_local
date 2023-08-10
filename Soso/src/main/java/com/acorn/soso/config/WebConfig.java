b5e32e3410af1fa3039402b6c455ad622c3dd23d
Wideds <why4517@naver.com>
2023-08-10 16:08:25 +0900

    23.08.10 회원가입 페이지 아이디 중복체크 기능 구현,문구 수정

--- Soso/src/main/java/com/acorn/soso/config/WebConfig.java
+++ Soso/src/main/java/com/acorn/soso/config/WebConfig.java
@@ -36,7 +36,8 @@ public class WebConfig implements WebMvcConfigurer{
 				.excludePathPatterns("/users/loginform", "/users/login", 
 						"/users/signup_form", "/users/signup",
 						"/users/pwd_findform","/users/pwd_find",
-						"/users/id_findform", "/users/id_find");
+						"/users/id_findform", "/users/id_find",
+						"/users/idCheck");
 		registry.addInterceptor(pwdAuthInter)
 				.addPathPatterns("/users/updateform", "/users/pwd_updateform");
 		registry.addInterceptor(loginInter)
--- Soso/src/main/java/com/acorn/soso/users/controller/UsersController.java
+++ Soso/src/main/java/com/acorn/soso/users/controller/UsersController.java
@@ -20,6 +20,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -39,6 +40,12 @@ public class UsersController {
 	public String signupForm() {
 		
 		return "users/signup_form";
+	}
+	
+	@PostMapping("/users/idCheck")
+	@ResponseBody
+	public boolean idCheck(@RequestParam String id) {
+	    return service.isExist(id);
 	}
 	
 	//회원 가입 요청처리
