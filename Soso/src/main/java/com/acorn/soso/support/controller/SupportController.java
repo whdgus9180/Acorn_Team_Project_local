b5944189d9941e98bae43a56ea30ceffbb485619
kariyarn <kariyarn@hanmail.net>
2023-08-16 18:01:10 +0900

    Httpsession import안되어있다 나와서 수정

--- Soso/src/main/java/com/acorn/soso/support/controller/SupportController.java
+++ Soso/src/main/java/com/acorn/soso/support/controller/SupportController.java
@@ -4,13 +4,13 @@ package com.acorn.soso.support.controller;
 
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 import com.acorn.soso.support.faq.dto.FaqDto;
 import com.acorn.soso.support.faq.dto.InquireDto;
