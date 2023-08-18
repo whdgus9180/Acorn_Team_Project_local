2e9d284504a1797f5d1a9b6accec6747776bf2fb
KOHHyeonJun <tttmmm100@naver.com>
2023-08-18 18:21:52 +0900

    8월18일 공지사항 등록 및 리스트 조회 기능 구현

--- Soso/src/main/java/com/acorn/soso/support/dao/NoticeDaoImpl.java
+++ Soso/src/main/java/com/acorn/soso/support/dao/NoticeDaoImpl.java
@@ -2,41 +2,52 @@ package com.acorn.soso.support.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.acorn.soso.support.dto.NoticeDto;
 
 @Repository
 public class NoticeDaoImpl implements NoticeDao {
-
+	
+	@Autowired
+	private SqlSession session;
+	
 	@Override
 	public List<NoticeDto> getList(NoticeDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return session.selectList("notice.getList",dto);
 	}
 
 	@Override
 	public void insert(NoticeDto dto) {
-		// TODO Auto-generated method stub
 		
+		session.insert("notice.insert",dto);
 	}
 
 	@Override
-	public NoticeDto getData(int num) {
-		// TODO Auto-generated method stub
-		return null;
+	public NoticeDto getData(int notice_num) {
+		
+		return session.selectOne("notice.getData",notice_num);
 	}
 
 	@Override
 	public void update(NoticeDto dto) {
-		// TODO Auto-generated method stub
 		
+		session.update("notice.update",dto);
 	}
 
 	@Override
-	public void delete(int num) {
-		// TODO Auto-generated method stub
+	public void delete(int notice_num) {
 		
+		session.delete("notice.delete",notice_num);
+	}
+
+	@Override
+	public int getCount(NoticeDto dto) {
+		
+		return session.selectOne("notice.getCount",dto);
 	}
 
 }
