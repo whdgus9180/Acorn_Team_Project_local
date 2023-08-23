ba0970526f53bbd93af977b0958886d6fc3ec618
Lee-Jin-Young <dlwlsdud618@naver.com>
2023-08-23 09:40:53 +0900

    bookmate.life만 입력해도 접속이 가능하도록 context path 삭제

--- Soso/src/main/java/com/acorn/soso/HomeController.java
+++ Soso/src/main/java/com/acorn/soso/HomeController.java
@@ -1,30 +1,21 @@
 package com.acorn.soso;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.acorn.soso.group.service.GroupService;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private GroupService service;
-	
 	@GetMapping("/")
 	public String home(HttpServletRequest request) {
-		List<String> list = new ArrayList<String>();
-		list.add("사용자가 처음 접하는 페이지");
-		
-		request.setAttribute("list", list);
-		
-		return "home";
+		return "redirect:main";
+	}
+	
+	@GetMapping("/main")
+	public String main() {
+		return "main";
 	}
 	
 }
